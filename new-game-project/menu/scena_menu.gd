0024b824f03c7727664927e88c4f8f3e4f741d83
jkonojacki <jkonojacki@DOM.SCI.EDU.PL>
2025-11-21 23:13:18 +0100

    Menu z intro i muzyka

--- new-game-project/menu/scena_menu.gd
+++ new-game-project/menu/scena_menu.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+@export var gotowy: bool = false
+
 var menuItems: Array[MenuItem] = []
+var animator: AnimationPlayer = null
+var muzykaIntro: AudioStreamPlayer = null
+var muzykaPetla: AudioStreamPlayer = null
 
 var pozycja = 0
 var funkcje: Array[Callable] = [
@@ -13,14 +18,27 @@ func graj():
 func wyjdz():
 	get_tree().quit()
 
+func muzyka_zapetlij():
+	muzykaPetla.play()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	menuItems.append(get_node("ItemGraj"))
-	menuItems.append(get_node("ItemWyjdz"))
+	menuItems.append(get_node("Menu/ItemGraj"))
+	menuItems.append(get_node("Menu/ItemWyjdz"))
+	
+	animator = get_node("Animator")
+	
+	muzykaIntro = get_node("MuzykaIntro")
+	muzykaPetla = get_node("MuzykaPetla")
 	
 	menuItems[pozycja].wybrany = true
+	animator.play("Intro")
+	muzykaIntro.connect("finished", muzyka_zapetlij)
 
 func _input(event: InputEvent) -> void:
+	if !gotowy:
+		return
+	
 	if event.is_action_pressed("ui_left") || event.is_action_pressed("ui_right"):
 		pozycja += 1
 		pozycja %= 2
