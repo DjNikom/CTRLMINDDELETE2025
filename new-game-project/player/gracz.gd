ff4b30fb910822485a565e9a066446b05d23233d
DjNikom <nikodemlis2010@gmail.com>
2025-11-21 23:16:23 +0100

    guwno

--- new-game-project/player/gracz.gd
+++ new-game-project/player/gracz.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var max_health: int = 100
+var current_health: int = max_health
+
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +27,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+signal health_changed(new_health)
+signal died
+
+func take_damage(amount: int):
+	current_health -= amount
+	if current_health < 0:
+		current_health = 0
+		emit_signal("health_changed", current_health)
+		if current_health == 0:
+			emit_signal("died")
+
+func heal(amount: int):
+	current_health += amount
+	if current_health > max_health:
+		current_health = max_health
+	emit_signal("health_changed", current_health)
