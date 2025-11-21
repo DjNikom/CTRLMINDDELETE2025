8e15381646a8d8646d5d5a727c600b87d5688227
Hubert <hub.grzybek@gmail.com>
2025-11-21 19:07:30 +0100

    l

--- new-game-project/Oponenci/oponent_wrecz.gd
+++ new-game-project/Oponenci/oponent_wrecz.gd
@@ -3,17 +3,29 @@ extends Node2D
 var ifGraczIn = false
 var atak
 var graczhp
+var atakDelay
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is gracz: 
+	if body is Gracz: 
 		ifGraczIn = true
 	
 	
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is gracz:
+	if body is Gracz:
 		ifGraczIn = false
 		
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if ifGraczIn == true:
 		graczhp -= atak
+		atakDelay = 5
+	
+func _physics_process(delta: float) -> void:
+	if atakDelay > 0:
+		atakDelay -= get_process_delta_time()
+	
+		
+		
+	
+	
+		
 		
