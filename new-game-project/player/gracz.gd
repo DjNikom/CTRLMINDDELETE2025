4c100171ecea528161b623baba0e4998908a032c
Mint <mici5dabestpl@gmail.com>
2025-11-21 23:52:30 +0100

    Dodano coś do startu i gracza.

--- new-game-project/player/gracz.gd
+++ new-game-project/player/gracz.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var djumped = false
 
 func _ready() -> void:
 	$AnimatedSprite2D.frame = 1
@@ -11,15 +12,23 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		djumped = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept") and !djumped:
+		djumped = true
+		if $CPUParticles2D.emitting:
+			$CPUParticles2D.emmiting = false
+		$CPUParticles2D.emitting = true
+		$AudioStreamPlayer2D.play()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	print(direction)	
 	if direction:
 		if not $AnimatedSprite2D.is_playing():
 			$AnimatedSprite2D.play("default")
