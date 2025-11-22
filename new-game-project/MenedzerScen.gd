36e788c4da3a03bc0c7435e82798cc6c98f57de0
jkonojacki <jkonojacki@DOM.SCI.EDU.PL>
2025-11-22 04:28:32 +0100

    Lepszy dzwiek w przejsciach

--- new-game-project/MenedzerScen.gd
+++ new-game-project/MenedzerScen.gd
@@ -1,11 +1,33 @@
 extends Node
 
+var glosnosc: int = 255
+var glosnosc_cel: int = glosnosc
+var glosnosc_delta: int = 1
+
+func ustawGlosnosc(g: float):
+	AudioServer.set_bus_volume_linear(
+		AudioServer.get_bus_index("Master"),
+		g
+	)
+
+func _process(_delta: float) -> void:
+	if glosnosc > glosnosc_cel:
+		glosnosc -= glosnosc_delta
+		ustawGlosnosc(glosnosc / 255.0)
+		if glosnosc < glosnosc_cel: glosnosc = glosnosc_cel
+	elif glosnosc < glosnosc_cel:
+		glosnosc += glosnosc_delta
+		ustawGlosnosc(glosnosc / 255.0)
+		if glosnosc > glosnosc_cel: glosnosc = glosnosc_cel
+
 func przejscie(scena: String):
 	var s = get_tree().current_scene
 	var p = preload("res://inne/Przejscie.tscn").instantiate()
 	s.add_child(p)
 	
 	var callback = func(_anim): animacja_koniec(scena)
+	glosnosc_cel = 0
+	glosnosc_delta = 4
 	
 	var animator = p.get_node("Animator")
 	animator.play("przejscie_start")
@@ -21,6 +43,7 @@ func animacja_koniec(scena: String):
 	s.add_child(p)
 	
 	var callback = func(_anim): przejscie_koniec(p)
+	glosnosc_cel = 255
 	
 	var animator = p.get_node("Animator")
 	animator.play("przejscie_koniec")
@@ -28,3 +51,14 @@ func animacja_koniec(scena: String):
 
 func przejscie_koniec(p: Node):
 	p.queue_free()
+
+func wyjdz():
+	var s = get_tree().current_scene
+	var p = preload("res://inne/Przejscie.tscn").instantiate()
+	s.add_child(p)
+	
+	var callback = func(_anim): get_tree().quit()
+	
+	var animator = p.get_node("Animator")
+	animator.play("przejscie_start")
+	animator.connect("animation_finished", callback.call)
