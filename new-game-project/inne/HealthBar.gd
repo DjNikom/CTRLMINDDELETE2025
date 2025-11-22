415bdd3acebb1d81f72d82b02b3e9cbae964824d
Mint <mici5dabestpl@gmail.com>
2025-11-22 09:24:41 +0100

    health bar

--- new-game-project/inne/HealthBar.gd
+++ new-game-project/inne/HealthBar.gd
@@ -1,12 +1,13 @@
 extends ProgressBar # Zmień klasę bazową na ProgressBar
-
+@onready var player = get_tree().get_nodes_in_group("Gracz")[0]
 func _ready():
 	# Znajdź węzeł gracza i połącz sygnał
-	var player = get_tree().get_first_node_in_group("Player")
+	
 	if player:
 		# Pamiętaj, aby nazwać funkcję docelową tak samo jak w sygnale
 		player.connect("health_changed", Callable(self, "_on_player_health_changed"))
 		# Ustaw początkową wartość paska
+		print(player.current_health)
 		self.value = player.current_health # Użyj self.value, aby odnieść się do właściwości paska
 
 func _on_player_health_changed(new_health):
